11a24738dcfb659b607788839a97ca0463a86901
mukeshsps <39326930+mukeshsps@users.noreply.github.com>
2018-05-16 16:27:55 +0530

    Update urls.py

--- EMall/urls.py
+++ EMall/urls.py
@@ -16,22 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from api import views as auth_views
-#from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^login/$', auth_views.login, name='login'),
-    #url(r'^login/$', LoginView.as_view(), name='login'),
 ]
 admin.site.site_header = ("E-Mall Site Administration")
 admin.site.site_title = ("My E-Mall Site Admin")
-#admin.site.site_header = "E_Mall Site Administration"
-#admin.site.site_title = "E-Mall Site Admin"
-
-#admin.site.site_header = 'Coffeehouse admin'
-#admin.site.site_title = 'Coffeehouse admin'
 admin.site.site_url = 'https://www.facebook.com'
 admin.site.index_title = 'Mukesh site'
-#admin.empty_value_display = '**Empty**
