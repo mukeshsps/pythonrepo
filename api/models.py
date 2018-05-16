ace8097f40daeccfd7f914f02fe26ad427a62648
mukeshsps <39326930+mukeshsps@users.noreply.github.com>
2018-05-16 16:23:25 +0530

    Update models.py

--- api/models.py
+++ api/models.py
@@ -108,13 +108,7 @@ class Mall(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
     
-    #def save(self, *args, **kwargs):
-        #"""if not self.id:  
-         #   super(Mall, self).save(*args, **kwargs)  
-          #  resized = get_thumbnail(self.M_image, "100x100")
-           # self.M_image.save(resized.name, ContentFile(resized.read()), True)
-        #super(Mall, self).save(*args, **kwargs)"""
-
+   
     def delete(self, *args, **kwargs):
         storage, path = self.M_image.storage, self.M_image.path
         storage1, path1 = self.thumb_pic.storage, self.thumb_pic.path
@@ -217,11 +211,7 @@ class Order(models.Model):
         return self.customer_name
     
 
-"""class User(AbstractUser, PermissionsMixin):
-    User = get_user_model()
-    name = models.CharField(max_length=100, blank=True, null=True)
-    address = models.CharField(max_length=100, blank=True, null=True)
-    phone_number = models.CharField(max_length=100, blank=True, null=True)"""
+
 
 
 class UserProfile(models.Model):
@@ -232,80 +222,7 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return self.full_name
-    
-
-"""class User(AbstractUser):
-    full_name = models.CharField(max_length=255)
-    address = models.TextField()
-    phone_number =models.CharField(max_length=255)
-    class Meta:
-        db_table = 'auth_user'"""
-        
-"""class CustomUserManager(BaseUserManager):
-    def _create_user(self, email, password,
-                     is_staff, is_superuser, **extra_fields):
-        
-        #Creates and saves a User with the given email and password.
-        
-        now = timezone.now()
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email,
-                          is_staff=is_staff, is_active=True,
-                          is_superuser=is_superuser, last_login=now,
-                          date_joined=now, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email, password=None, **extra_fields):
-        return self._create_user(email, password, False, False,
-                                 **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        return self._create_user(email, password, True, True,
-                                 **extra_fields)
-        
-        
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    User = get_user_model()
-    email = models.EmailField(('email address'), max_length=254, unique=True)
-    first_name = models.CharField(('first name'), max_length=30, blank=True)
-    last_name = models.CharField(('last name'), max_length=30, blank=True)
-    address = models.CharField(('address'), max_length=255, blank=True)
-    phone = models.CharField(('phone'), max_length=255, blank=True)
-    is_staff = models.BooleanField(('staff status'), default=False,
-        help_text=('Designates whether the user can log into this admin '
-                    'site.'))
-    is_active = models.BooleanField(('active'), default=True,
-        help_text=('Designates whether this user should be treated as '
-                    'active. Unselect this instead of deleting accounts.'))
-    date_joined = models.DateTimeField(('date joined'), default=timezone.now)
-
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    class Meta:
-        verbose_name = ('user')
-        db_table = 'auth_user'
-        verbose_name_plural = ('users')
-
-    def get_absolute_url(self):
-        return "/users/%s/" % urlquote(self.email)
-
-    def get_full_name(self):
-        full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
-
-    def get_short_name(self):
-        return self.first_name
-
-    def email_user(self, subject, message, from_email=None):
-        send_mail(subject, message, from_email, [self.email])"""
-     
+         
 class User(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     full_name = models.CharField(max_length=255, null=True, blank=True)
